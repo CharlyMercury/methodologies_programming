1228f2d1003d2525213887eb8d692afd54e18d30
Charly Mercury <carlos.mercury92@gmail.com>
2024-10-15 13:47:40 -0600

    Hoy aprendí sobre el recorrido en listas

---
+++ src/06_understanding_how_to_work_with_lists.py
@@ -0,0 +1,8 @@
+magicians = ["Merlin", 'Voldemort', "Harry Potter", "Hagrid"]
+print(magicians)
+for mago in magicians:
+    print(mago)
+    print(mago.upper()+" Ese fue un gran hechizo.")
+    print(f"No puedo esperar para ver tu siguiente hechizo, {mago.lower()} \n")
+print("Gracias a todos los magos. Fue un gran espectáculo.")
+    
--- src/understanding_how_to_work_with_lists.py
+++
@@ -1,7 +0,0 @@
-# Vamos a aprender como recorrer una lista
-magicians = ['alan', 'winie', 'odin', 'houdini', 'Zeus']
-for mago in magicians:
-    print(mago)
-    print(mago.title()+" hizo un gran hechizo")
-    print(f"No puedo esperar para ver tu siguiente hechizo {mago.upper()}\n")
-print("Gracias a todos. Fue un gran espectáculo")
