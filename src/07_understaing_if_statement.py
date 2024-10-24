e3b61994517ddd07d3d273b4d137381915459a87
Charly Mercury <carlos.mercury92@gmail.com>
2024-10-24 12:03:06 -0600

    If and dictionaries

--- src/07_understaing_if_statement.py
+++ src/07_understaing_if_statement.py
@@ -1,6 +1,118 @@
-cars = ['bocho', 'mustang', 'sentra', 'honda', 'gtr']
+
+cars = ['bocho', 'mustang', 'sentra', 'honda', 'gtr', 'mustang']
+
 for car in cars:
     if car == 'mustang':
-        print(car.upper())
+        pass
+        # print(car.upper())
     else:
-        print(car.title())
+        pass
+        # print(car.title())
+
+
+"""
+    Condicionales - Los condicionales
+    son el corazón de un if
+"""
+
+# Condicional que tiene como resultado True
+car = 'bmw'
+print(car == 'bmw')
+
+# Condicional que tiene como resultado False
+car = 'Audi'
+print(car=='audi')
+
+# Condicional que tiene como resultado True
+car = 'Audi'
+print(car.lower()=='audi')
+
+# Condicional != para determinar desigualdad
+tacos = 'bistek'
+print(tacos!='pastor')
+
+
+## 
+age = 18
+print(age==18)
+
+answer = 17
+print(answer!=42)
+
+
+age = 21
+print(age<21)
+print(age<=21)
+print(age>21)
+print(age>=21)
+
+"""
+    Multiple Conditions
+"""
+age_0 = 22
+age_1 = 18
+print(age_0>=21 or age_1>=21) # -> arroja True
+print(age_0>=21 and age_1>=21) # -> arroja False
+
+print("Don Charly")
+####
+## if -elif - else
+"""
+    Costo para una persona: 
+        <= 4 años -> entrada gratis
+        <= 18 -> paga $200
+        mayor de 18 -> $500
+"""
+age = 40
+if age <= 4:
+    price = 0
+elif age <= 18:
+    price = 200
+elif age <= 50:
+    price = 500
+elif age > 50: 
+    price = 100
+    
+print(f"Tu pagas {price} pesos.")
+
+
+### Multiple conditions
+guisos = ['deshebrada', 'salsa verde']
+if 'deshebrada' in guisos:
+    print('Hay deshebrada')
+elif 'salsa verde' in guisos:
+    print('Hay salsa verde')
+elif 'picadillo' in guisos:
+    print('Hay picadillo')
+    
+print("Éstos guisos hay")
+print()
+
+print()
+
+
+#### Listas Vacías
+
+
+if guisos:
+    print("Tengos guisos") 
+else:
+    print("No hay guisos")
+    
+    
+####
+guisos_availables = ['salsa verde', 'deshebrada', 'picadillo',
+                   'huevo chorizo']
+
+guisos_to_order = ['barbacoa', 'deshebrada', 'cabrito', 'picadillo']
+
+print("¿Qué desea ordenar?")
+
+for guiso in guisos_to_order:
+    if guiso in guisos_availables:
+        print(f"Si tenemos {guiso}")
+    else: 
+        print(f"No tenemos {guiso}")
+
+print("Realizar Pedido")
+
---
+++ src/08_understanding_dictionaries.py
@@ -0,0 +1,7 @@
+"""
+    
+"""
+covenant_1 = {'color': 'blue', 'points': 10}
+print(covenant_1['color'])
+print(covenant_1['points'])
+
