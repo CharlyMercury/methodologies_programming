2d33ff77f6004133ed66e158952773f14c9031d1
Charly Mercury <carlos.mercury92@gmail.com>
2024-10-25 12:52:05 -0600

    Clase de dictionaries clase 105

--- src/08_understanding_dictionaries.py
+++ src/08_understanding_dictionaries.py
@@ -13,14 +13,14 @@ print(covenant_elite)
 # How to add elements to a dictionary
 covenant_elite['x-position'] = 0
 covenant_elite['y-position'] = 25
-
+covenant_elite['weapon'] = "sword"
 print(covenant_elite)
 
 # Empty Dictionary
 covenant_grunt = {}
 print(type(covenant_grunt))
 
-covenant_grunt['color'] = 'red-orange'
+covenant_grunt['color'] = 'orange'
 covenant_grunt['points'] = 1
 
 print(covenant_grunt)
@@ -32,7 +32,7 @@ del covenant_grunt['points']
 
 covenant_grunt['x-position'] = 5
 covenant_grunt['y-position'] = 25
-covenant_grunt['speed'] = 'super_slow'
+covenant_grunt['speed'] = 'slow'
 
 print(covenant_grunt)
 
@@ -46,3 +46,45 @@ else:
 covenant_grunt['x-position'] = covenant_grunt['x-position'] + x_increment
 
 print(covenant_grunt)
+
+
+programming_lenguages = {
+    'alan': 'python',
+    'valente': 'c++',
+    'ariel': 'c',
+    'yepez': 'Python',
+}
+print(programming_lenguages['valente'].title())
+
+for nombre, lenguage in programming_lenguages.items():
+    print(f"{nombre.title()} le gusta el lenguage: ", lenguage)
+
+"""
+    Keys
+"""
+friends = ['ariel', 'yepez', 'alan', 'valente']
+for name in programming_lenguages.keys():
+    if name in friends:
+        print(f" {name} Hola es un gusto saber que te gusta: {programming_lenguages[name]}" )
+
+if 'charly' not in programming_lenguages.keys():
+    print("A charly no le gusta programar")
+    
+print(programming_lenguages.keys())
+
+for key in programming_lenguages.keys():
+    print(key)
+
+for key in sorted(programming_lenguages.keys()):
+    print(key)
+
+print()    
+
+for lenguaje in programming_lenguages.values():
+    print(lenguaje)
+    
+print()
+
+
+for lenguaje in set(programming_lenguages.values()):
+    print(lenguaje)
